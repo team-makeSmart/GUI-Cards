21227c53fe15bf579633eb2115660e0e8a7e579c
p12345vls <32381448+p12345vls@users.noreply.github.com>
2018-03-30 11:53:53 -0700

    Update Phase2.java

--- src/Phase2.java
+++ src/Phase2.java
@@ -1,66 +1,146 @@
+import java.awt.*;
 import javax.swing.*;
-
+import javax.swing.border.*;
 
 public class Phase2
 {
-   public static void main(String[] args)
-   {
-      Deck deck = new Deck();
-      Hand hand = new Hand();
-      
-      deck.shuffle();
-      
-      while(deck.getNumCards() > 0)
-      {
-         hand.takeCard(deck.dealCard());
-      }
-      System.out.println("Shuffled");
-      System.out.println(hand.toString());
-      System.out.println();
-      
-      hand.sort();
+	static int NUM_CARDS_PER_HAND = 7;
+	static int NUM_PLAYERS = 2;
+	static JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
+	static JLabel[] humanLabels = new JLabel[NUM_CARDS_PER_HAND];
+	static JLabel[] playedCardLabels = new JLabel[NUM_PLAYERS];
+	static JLabel[] playLabelText = new JLabel[NUM_PLAYERS];
 
-      System.out.println(hand.toString());
-      //JLabel myLabel = new JLabel( "My Text", JLabel.CENTER );
-      //Card card = new Card('X', Card.Suit.CLUBS);
-      //System.out.println(card.getValue() + " -> " + card.valueAsInt());
-   }
+	public static void main(String[] args)
+	{
+		int k;
+		Icon tempIcon;
+
+		// establish main frame in which program will run
+		CardTable myCardTable = new CardTable("CardTable", NUM_CARDS_PER_HAND, NUM_PLAYERS);
+		myCardTable.setSize(800, 600);
+		myCardTable.setLocationRelativeTo(null);
+		myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GUICard.loadCardIcons();
+
+		Deck deck = new Deck();
+
+		deck.shuffle();
+
+		for (int i = 0; i < NUM_CARDS_PER_HAND; i++)
+		{
+			myCardTable.pnlComputerHand.add(new JLabel(GUICard.getBackCardIcon()));
+			myCardTable.pnlHumanHand.add(new JLabel(GUICard.getIcon(deck.dealCard())));
+		}
+
+		// CREATE LABELS ----------------------------------------------------
+		// code goes here ...
+
+		// ADD LABELS TO PANELS -----------------------------------------
+		// code goes here ...
+
+		// and two random cards in the play region (simulating a computer/hum
+		// ply)
+		// code goes here ...
+
+		// show everything to the user
+		myCardTable.setVisible(true);
+	}
 }
 
+/**
+ * Controls the positioning of the panels and cards of the GUI
+ */
 class CardTable extends JFrame
 {
-   static int MAX_CARDS_PER_HAND = 56;
-   static int MAX_PLAYERS = 2; // for now, we only allow 2 person games
+	static int MAX_CARDS_PER_HAND = 56;
+	static int MAX_PLAYERS = 2; // for now, we only allow 2 person games
 
-   private int numCardsPerHand;
-   private int numPlayers;
+	private int numCardsPerHand;
+	private int numPlayers;
 
-   public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+	public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
 
-   CardTable(String title, int numCardsPerHand, int numPlayers)
-   {
-   }
+	/**
+	 * Instantiates a new card table.
+	 *
+	 * @param title
+	 *            the title
+	 * @param numCardsPerHand
+	 *            the num cards per hand
+	 * @param numPlayers
+	 *            the num players
+	 */
+	CardTable(String title, int numCardsPerHand, int numPlayers) {
+		super(title);
+		// create a default Font style
+		UIManager.getDefaults().put("TitledBorder.font", (new Font("Arial", Font.BOLD, 14)));
+
+		// three rows, zero columns layout, 10 pixels space
+		setLayout(new GridLayout(3, 0, 10, 10));
+
+		// create the three panels with title borders
+		pnlComputerHand = new JPanel();
+		pnlComputerHand.setBorder(BorderFactory.createTitledBorder("Computer Hand"));
+		pnlPlayArea = new JPanel();
+		pnlPlayArea.setBorder(BorderFactory.createTitledBorder("Playing Area"));
+		pnlHumanHand = new JPanel();
+		pnlHumanHand.setBorder(BorderFactory.createTitledBorder("Your Hand"));
+
+		// add the panels to the JFrame
+		add(pnlComputerHand);
+		add(pnlPlayArea);
+		add(pnlHumanHand);
+
+	}
 }
 
-class GUICard
-{
-   private static Icon[][] iconCards = new ImageIcon[14][4]; // 14 = A thru K + joker
-   private static Icon iconBack;
-   static boolean iconsLoaded = false;
-   
-   static void loadCardIcons()
-   {
-      
-   }
-   
-   static public Icon getIcon(Card card)
-   {
-      return iconCards[Card.valueAsInt(card)][Card.suitAsInt(card)];
-   }
-   
-   static public Icon getBackCardIcon()
-   {
-      return iconBack;
-   }
-}
+/**
+ * Manages the reading and building of the card image Icons
+ */
+class GUICard {
+	private static Icon[][] iconCards = new ImageIcon[14][4]; // 14 = A thru K +
+																// joker
+	private static Icon iconBack;
+	static boolean iconsLoaded = false;
 
+	static void loadCardIcons()
+	{
+		if (iconCards[0][0] != null)
+		{
+			iconsLoaded=true;
+			return;
+		}
+		else
+		{
+			String inputFileName = "src/images/";
+			String fileExtension = ".gif";
+
+			for (int i = 0; i < iconCards.length ; i++)
+			{
+				for (int j = 0; j <  iconCards[i].length ; j++)																
+				{
+					iconCards[i][j] = new ImageIcon(inputFileName + Phase1.turnIntIntoCardValue(i)
+							+ Phase1.turnIntIntoCardSuit(j) + fileExtension);
+				}
+			}
+			//set the last card of the array as back card
+			iconCards[13][3]=new ImageIcon(inputFileName+"BK"+fileExtension);
+			iconBack = iconCards[13][3];
+		}
+		
+		//testing
+		System.out.println(iconBack);
+		System.out.println(getIcon(new Card('4', Card.Suit.CLUBS)));
+
+	}
+
+	static public Icon getIcon(Card card) {
+		return iconCards[Card.valueAsInt(card)][Card.suitAsInt(card)];
+	}
+
+	static public Icon getBackCardIcon() {
+		return iconBack;
+	}
+}
